45b9906e50673cd240c5903111d450512e96c3f1
Leopaski <leonardo.pascale@gmail.com>
2023-05-17 15:37:13 +0200

    commit math.random

---
+++ js/myscript.js
@@ -0,0 +1,17 @@
+const usernumber= ;
+const computernumber= ;
+function getRandomArbitrary(min, max) {
+    return usernumber=Math.random() * (1 - 6) + 1;
+  }
+  function getRandomArbitrary(min, max) {
+    return computernumber=Math.random() * (1 - 6) + 1;
+  }
+if (computernumber>usernumber) {
+    console.log ('hai perso!')
+} else if (computernumber<usernumber) {
+    console.log ('daje,hai vinto')
+}else {
+    console.log ('avete pareggiato')
+}
+
+  
